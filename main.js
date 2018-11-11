0fa943b2f97a0c04b880f90b43a30b6981104830
Your Name <you@example.com>
2018-11-11 12:27:32 -0500

    added addwindow to add task to list

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const path = require('path');
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let addWindow;
 
 // Listen for app to be ready 
 app.on('ready', function(){
@@ -24,13 +25,32 @@ app.on('ready', function(){
 	Menu.setApplicationMenu(mainMenu);
 });
 
+// Create add window
+function createAddWindow(){
+	// Create new window
+	addWindow = new BrowserWindow({
+		width: 300,
+		height: 200,
+		title: 'Add Task to To Do List'
+	});
+	// Load html into window
+	addWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'addWindow.html'),
+		protocol:'file:', 
+		slashes: true
+	}));
+}
+
 // Create menu template
 const mainMenuTemplate = [
 	{
 		label: 'File',
 		submenu:[
 			{
-				label: 'Add Task'
+				label: 'Add Task',
+				click(){
+					createAddWindow();
+				}
 			},
 			{
 				label: 'Clear Task'
